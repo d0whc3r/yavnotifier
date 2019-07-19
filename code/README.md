3a8f4db6d7c0e25cc9445143d7876b30b9ef0569
d0whc3r <d0whc3r@gmail.com>
2019-07-19 02:41:52 +0200

    chore: add documentation

---
+++ code/README.md
@@ -0,0 +1,12 @@
+# YavNotifier
+Yet another version notifier
+
+# Install
+
+```bash
+$ npm i -D @d0whc3r/yavnotifier
+```
+
+# Usage
+
+[See documentation](./docs)
--- code/src/index.ts
+++ code/src/index.ts
@@ -4,15 +4,33 @@ import boxen from 'boxen';
 import * as semver from 'semver';
 import chalk from 'chalk';
 
+/**
+ * Get the latest version of a package
+ * @param {string} name Name of the package to get the latest version
+ * @returns {Promise<string>} Promise with latest version as result
+ */
 export async function latest(name: string) {
   return latestVersion(name);
 }
 
+/**
+ * Check if a package and version is the latest
+ * @param {string} name Name of the package to check
+ * @param {string} version Version to check if it is the latest
+ * @returns {Promise<boolean>} Promise with boolean true if its the latest version or false otherwise
+ */
 export async function isUpdated(name: string, version: string) {
   const lversion = await latest(name);
   return semver.eq(lversion, version);
 }
 
+/**
+ * Show a notification if there is a newest version for package
+ * @param {string} name Name of the package to check
+ * @param {string} version Version to check if it is the latest
+ * @param {string} message (optional) Message to show in the notification
+ * @returns {Promise<void>}
+ */
 export async function notify(name: string, version: string, message: string = 'Update available for package') {
   try {
     const lversion = await latest(name);
